abe4f7cfe9469829a485a1f5648114a7520ad896
joanlopbor <joanlopbor.info4@gmail.com>
2021-02-23 08:54:10 +0100

    i Please enter the commit message for your changes. Lines starting

--- BirthDayProgrammerJoanLopez/src/p21_terminal/P21_terminal.java
+++ BirthDayProgrammerJoanLopez/src/p21_terminal/P21_terminal.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Author: Joan Lopez
+Date: 23/02/2021
+Piano
  */
 package p21_terminal;
 
